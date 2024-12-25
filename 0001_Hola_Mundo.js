68d46fb8628a1672974a5041881e156031742965
kabalera82 <kabalera@gmail.com>
2024-12-25 21:20:21 +0100

    Initial commit

---
+++ 0001_Hola_Mundo.js
@@ -0,0 +1,12 @@
+// Ejecutar con: node 0001_Hola_Mundo.js
+
+console.log("Hola Mundo!");
+
+// Hola Mundo!
+
+let hola = "Hola";
+let mundo = "Mundo";
+
+console.log(hola + " " + mundo + "!");
+
+// Hola Mundo!
