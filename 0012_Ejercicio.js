0bfc5f8eda2ad5019544d7b627853cb56001a6e2
kabalera82 <kabalera@gmail.com>
2025-01-13 06:28:03 +0100

    Reapply "dfghj"
    This reverts commit ae4dd5e4938d1baca562955076c05da5080f8664.

--- 0012_Ejercicio.js
+++
@@ -1,6 +0,0 @@
-/*
- * Frutero necesita calcular beneficios anuales que obtiene de la 
- * venta de manzanas y peras. Necesita aplicación que solicite ventas en
- * (kilos) de cada semestre para fruta, mostrará el importe sabiendo
- * que el precio del kilo está fijado en 2,35 para manzanas y 1,95 para peras.
- */
